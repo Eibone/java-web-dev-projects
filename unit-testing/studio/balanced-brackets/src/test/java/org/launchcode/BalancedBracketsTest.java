cac56e930402d4af687577a696279610010cef36
William R <wrowe31@yahoo.com>
2023-09-11 20:22:27 -0500

    Studio 5 TDD

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,61 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void balancedBracketsInDifferentOrders () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void nestedBracketsReturnTrue () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[Nested]]"));
+    }
+
+    @Test
+    public void nestedOnNestedBracketsReturnTrue () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[Launch[Code]]]"));
+    }
+    @Test
+    public void onlyUnbalancedBracketsReturnFalse () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void bracketsBalancedWithEveryOtherCharReturnTrue () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("L[A]U[N]C[H]"));
+    }
+    @Test
+    public void bracketsWithSpacesReturnTrue () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[ LaunchCode ]"));
+    }
+    @Test
+    public void bracketsWithUnbalancedNestedReturnFalse () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[Nes][ted]"));
+
+    }
+    @Test
+    public void bracketsWithSpecialCharactersReturnTrue () {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("Special[{Character}]"));
+    }
+
+    @Test
+    public void nestedEmptyBalancedBracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][]"));
+    }
+    @Test
+    public void nestedEmptyUnbalancedBracketsReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][][]["));
+    }
+
 }
